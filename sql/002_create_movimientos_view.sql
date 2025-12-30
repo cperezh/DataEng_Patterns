6a047b1044f2979d06c60ae827f381705a1e721f
Carlos Perez <cperezh@gmail.com>
2025-12-30 10:12:12 +0100

    missing ; on query

--- sql/002_create_movimientos_view.sql
+++ sql/002_create_movimientos_view.sql
@@ -1,6 +1,6 @@
 CREATE MATERIALIZED VIEW bancapp.movimientos_mview AS
     select distinct fecha_valor, importe, saldo
-    from movimientos_staging
+    from movimientos_staging;
 
 CREATE UNIQUE INDEX idx_movimientos_composite 
 ON bancapp.movimientos_mview (fecha_valor, importe, saldo);
