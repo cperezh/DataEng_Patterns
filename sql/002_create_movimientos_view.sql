aacda11b7bde808323bd3200150676e4853e7f0d
Carlos Perez <cperezh@gmail.com>
2025-12-30 10:13:16 +0100

    missing bancapp schema

--- sql/002_create_movimientos_view.sql
+++ sql/002_create_movimientos_view.sql
@@ -1,6 +1,6 @@
 CREATE MATERIALIZED VIEW bancapp.movimientos_mview AS
     select distinct fecha_valor, importe, saldo
-    from movimientos_staging;
+    from bancapp.movimientos_staging;
 
 CREATE UNIQUE INDEX idx_movimientos_composite 
 ON bancapp.movimientos_mview (fecha_valor, importe, saldo);
