133d3cc45746879ba24817fc1efde36b76ce1c78
Carlos Perez <cperezh@gmail.com>
2026-03-10 07:37:43 +0100

    fix ok

--- sql/008_new_mview_format_numeric.sql
+++ sql/008_new_mview_format_numeric.sql
@@ -1,39 +1,44 @@
 DROP MATERIALIZED VIEW bancapp.movimientos_mview;
 
 CREATE MATERIALIZED VIEW bancapp.movimientos_mview AS
-    select 
-        fecha_valor, 
-        cast(
-           -- Format numeric with group and decimal separators
-           case 
-             -- when ',' is the decimal separator
-             when importe LIKE '%,__' then replace(replace(importe,'.',''),',','.')
-             -- when '.' is the decimal separator
-             when importe LIKE '%.__' then replace(importe,',','') 
-           end as numeric) 
-        as importe, 
-        cast(
-           -- Format numeric with group and decimal separators
-           case 
-             -- when ',' is the decimal separator
-             when saldo LIKE '%,__' then replace(replace(saldo,'.',''),',','.')
-             -- when '.' is the decimal separator
-             when saldo LIKE '%.__' then replace(saldo,',','') 
-           end as numeric) 
-        as saldo, 
-        categoria, 
-        subcategoria, 
-        descripcion, 
-        created_at
-    from (
-        -- Ordenamos los movimientos por fecha de carga
-        select 
-        row_number() over (partition by fecha_valor, importe, saldo order by created_at desc) as row_id,
+    SELECT 
+      id,
+      fecha_valor,
+      importe,
+      saldo,
+      categoria,
+      subcategoria,
+      descripcion,
+      created_at
+    FROM
+      (SELECT 
+        row_number() OVER (
+          PARTITION BY temp.fecha_valor, temp.importe, temp.saldo 
+          ORDER BY temp.created_at DESC) 
+        AS row_id,
         *
-        from bancapp.movimientos_staging
-    )
-    -- Seleccionamos la version mas reciente del movimiento
-    where row_id = 1;
+      FROM 
+        -- CLEAN DATA
+        (SELECT      
+          id,
+          fecha_valor,
+          CASE
+              WHEN importe LIKE '%,__'::text THEN replace(replace(importe, '.'::text, ''::text), ','::text, '.'::text)
+              WHEN importe LIKE '%.__'::text THEN replace(importe, ','::text, ''::text)
+          END AS importe,
+          CASE
+              WHEN saldo LIKE '%,__'::text THEN replace(replace(saldo, '.'::text, ''::text), ','::text, '.'::text)
+              WHEN saldo LIKE '%.__'::text THEN replace(saldo, ','::text, ''::text)
+              ELSE NULL::text
+          END AS saldo,
+          created_at,
+          categoria,
+          subcategoria,
+          descripcion
+          FROM bancapp.movimientos_staging 
+        ) as temp
+      ) as final
+    WHERE row_id = 1;
 
 CREATE UNIQUE INDEX idx_movimientos_composite 
 ON bancapp.movimientos_mview (fecha_valor, importe, saldo);
